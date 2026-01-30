ed50602ba8c1bc4adf37ad1728291423fd1031a4
Susanne Rejaei <Susanne.rejaei@outlook.com>
2026-01-30 13:45:01 +0100

    add /encrypt endpoint with basic Caesar cipher

---
+++ CaesarCipher.cs
@@ -0,0 +1,21 @@
+public class CaesarCipher
+{
+//Kryptera med Caesar-chiffer
+public string Encrypt(string input, int shift)
+{
+    char [] buffer = input.ToCharArray(); //Array för krypterad text
+
+    for (int i = 0; i < buffer.Length; i++)
+    {
+        char letter = buffer[i];
+
+        if (char.IsLetter(letter))
+        {
+            char offset =char.IsUpper(letter) ? 'A' : 'a';
+            letter = (char)((letter -offset + shift + 26) % 26 + offset);
+        }
+        buffer[i]=letter;
+    }
+    return new string(buffer);
+}
+}
